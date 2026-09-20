77721f73d0d744dfe6d5a9c32f6a737cba6524ad
Meri <mehriaslanova59@gmail.com>
2026-09-20 17:27:51 +0500

    Add files via upload

---
+++ bd.sql
@@ -0,0 +1,38 @@
+CREATE TABLE Readers (
+    id_read INT PRIMARY KEY,
+    name VARCHAR(100),
+    phone VARCHAR(20)
+);
+
+CREATE TABLE Books (
+    id_bk INT PRIMARY KEY,
+    since VARCHAR(4),
+    name VARCHAR(200)
+);
+
+CREATE TABLE Authors (
+    id_auth INT PRIMARY KEY,
+    name VARCHAR(100)
+);
+
+CREATE TABLE Books_authors (
+    id_bk INT,
+    id_auth INT,
+
+    PRIMARY KEY (id_bk, id_auth),
+
+    FOREIGN KEY (id_bk) REFERENCES Books(id_bk),
+    FOREIGN KEY (id_auth) REFERENCES Authors(id_auth)
+);
+
+CREATE TABLE Takes (
+    id_take INT PRIMARY KEY,
+    id_read INT,
+    id_bk INT,
+    date_of_issue DATE,
+    return_date DATE,
+    issuance_timeframe INT,
+
+    FOREIGN KEY (id_read) REFERENCES Readers(id_read),
+    FOREIGN KEY (id_bk) REFERENCES Books(id_bk)
+);
